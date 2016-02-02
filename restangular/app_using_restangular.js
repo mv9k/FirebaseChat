8d755f0c738b79a7548e76c606a7c6176ae2c6d9
Kris <mv9kjw@gmail.com>
2016-02-02 08:37:16 -0700

    rectangular working

--- restangular/app_using_restangular.js
+++ restangular/app_using_restangular.js
@@ -6,8 +6,10 @@ app.factory("chatMessages", ["Restangular",
     function(Restangular) {
 
         var restangular = Restangular
-            .setBaseUrl('https://blinding-fire-7673.firebaseio.com/chatroom')
-            .setRequestSuffix('.json');
+            .setBaseUrl("https://blinding-fire-7673.firebaseio.com/")
+            .setRequestSuffix(".json")
+            .setFullResponse(true)
+            .service("chatroom");
 
 
         return restangular;
@@ -16,79 +18,63 @@ app.factory("chatMessages", ["Restangular",
 ]);
 
 
-app.controller("ChatCtrl", ["$scope", "chatMessages",
-    // we pass our new chatMessages factory into the controller
+app.controller("ChatCtrl", ["$scope", "chatMessages", "Restangular",
+
     function($scope, chatMessages) {
         $scope.user = "user";
         $scope.username = '';
         $scope.colorSelect = 0;
 
-        // we add chatMessages array to the scope to be used in our ng-repeat
-        $scope.messages = chatMessages.get();
 
-        // a method to create new messages; called by ng-submit
+        chatMessages.one().get().then(function(response) {
+            $scope.messages = response.data.plain();
+        });
+
+
         $scope.addMessage = function() {
-            // calling $add on a synchronized array is like Array.push(),
-            // except that it saves the changes to our database!
-            //$scope.messages.$add({
+
+
+            //chatMessages.save({
             //    from: $scope.username,
             //    content: $scope.message
+            //}, function() {
+            //    $scope.messages = chatMessages.get();
             //});
-
-            chatMessages.save({
-                from: $scope.username,
-                content: $scope.message
-            }, function() {
-                $scope.messages = chatMessages.get();
-            });
-
-
-            // reset the message input
-            $scope.message = "";
+            //
+            //
+            //
+            //$scope.message = "";
 
         };
 
         $scope.deleteMessage = function(key) {
-            console.log('deleting key=' + key);
-
-            $scope.entry = chatMessages.get({id: key}, function() {
-                $scope.entry.$delete({id: key}, function() {
-                    console.log('delete successful!');
-                    $scope.messages = chatMessages.get();
-                });
-            });
-
-            //(new chatMessages()).$delete({id: key}, function() {
+            //console.log('deleting key=' + key);
+            //
+            //$scope.entry = chatMessages.get({id: key}, function() {
             //    $scope.entry.$delete({id: key}, function() {
             //        console.log('delete successful!');
+            //        $scope.messages = chatMessages.get();
+            //    });
+            //});
+            //
+            //
+
+        };
+
+        $scope.updateMessage = function(key) {
+            //var newVal = window.prompt("New value:");
+            //console.log("new value for key = " + key + ': ' + newVal);
+            //
+            //$scope.entry = chatMessages.get({id: key}, function() {
+            //    $scope.entry.content = newVal;
+            //    $scope.entry.$update({id: key}, function() {
+            //        console.log('update successful!');
             //        $scope.messages = chatMessages.get();
             //    });
             //});
 
         };
 
-        $scope.updateMessage = function(key) {
-            var newVal = window.prompt("New value:");
-            console.log("new value for key = " + key + ': ' + newVal);
 
-            $scope.entry = chatMessages.get({id: key}, function() {
-                $scope.entry.content = newVal;
-                $scope.entry.$update({id: key}, function() {
-                    console.log('update successful!');
-                    $scope.messages = chatMessages.get();
-                });
-            });
-
-        };
-
-        //// if the messages are empty, add something for fun!
-        //$scope.messages.$loaded(function() {
-        //    if ($scope.messages.length === 0) {
-        //        $scope.messages.$add({
-        //            from: "Firebase Docs",
-        //            content: "Hello world!"
-        //        });
-        //    }
-        //});
     }
 ]);
